7f7056097ede1bd7c906f956005a0523e7cf1803
baligh8 <obaligh8@gmail.com>
2023-02-01 15:01:58 +0100

    changement main

--- src/ClassAymenMoulehi.java
+++ src/ClassAymenMoulehi.java
@@ -2,4 +2,8 @@ public class ClassAymenMoulehi {
     public String sayMyName(){
         return "Aymen Moulehi";
     }
+    public String MyName(){
+        return " helloo Aymen Moulehi";
+    }
+
 }
